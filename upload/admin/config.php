6d43e0463936e5c40e650d936d85eeface4f7a87
noguilt93 <dhananjaymuddappa@gmail.com>
2015-01-10 18:35:40 +0530

    COMMIT
    COMMIT 1

---
+++ upload/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\upload/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\upload/system/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\upload/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\upload/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\upload/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\upload/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\upload/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\upload/system/download/');
+define('DIR_UPLOAD', 'C:\xampp\htdocs\upload/system/upload/');
+define('DIR_LOGS', 'C:\xampp\htdocs\upload/system/logs/');
+define('DIR_MODIFICATION', 'C:\xampp\htdocs\upload/system/modification/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'krsnah');
+define('DB_PREFIX', 'oc_');
